ed08ae0762e1c40a07074c34c4be36a076f99054
henzlo <ekkaharshit556@gmail.com>
2026-06-20 11:25:34 +0530

    2.5 part 2 contain the Optional part how we are handle the using if and else

--- Restful_Api/src/main/java/com/Rest_api/Restful_Api/controllers/EmployeeController.java
+++ Restful_Api/src/main/java/com/Rest_api/Restful_Api/controllers/EmployeeController.java
@@ -1,15 +1,18 @@
 package com.Rest_api.Restful_Api.controllers;
 
 import com.Rest_api.Restful_Api.dto.EmployeeDTO;
-import com.Rest_api.Restful_Api.entities.EmployeeEntity;
-import com.Rest_api.Restful_Api.respositories.EmployeeRepository;
+//import com.Rest_api.Restful_Api.entities.EmployeeEntity;
+//import com.Rest_api.Restful_Api.respositories.EmployeeRepository;
 import com.Rest_api.Restful_Api.services.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 
 // create a parent url path
@@ -39,11 +42,14 @@ public class EmployeeController {
 
 
     //     path variable ----> used for the normal ids
-    @GetMapping("/{employeeID}")
-    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeID") Long id){
-//         return new EmployeeDTO(employeeID,  "Harh","harsh@gmial.com", 23,LocalDate.of(2024,1,12),true);
-        return employeeService.getEmployeeById(id);
 
+    @GetMapping("/{employeeID}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById (@PathVariable(name = "employeeID") Long id){
+        // optinal help us to represent a value that may be prsent or not
+        Optional<EmployeeDTO> employeeDTO = employeeService.getEmployeeById(id);
+        return employeeDTO
+                .map(employeeDTO1 -> ResponseEntity.ok(employeeDTO1))
+                .orElse(ResponseEntity.notFound().build());
 
     }
 
@@ -51,8 +57,8 @@ public class EmployeeController {
     // Requestparam --> you need get all the employee and short or filter those in based of other thing like age
 
     @GetMapping
-    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy){
-        return employeeService.getAllEmployees();
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy){
+        return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
     // postmapping
@@ -63,25 +69,31 @@ public class EmployeeController {
 
     // postmapping using the requestbody
     @PostMapping
-    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
-        return employeeService.createNewEmployee(inputEmployee);
+    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+        EmployeeDTO saveEmployee=  employeeService.createNewEmployee(inputEmployee);
+        return  new ResponseEntity<>(saveEmployee, HttpStatus.CREATED);
     }
     // putmapping---------
     @PutMapping("/{employeeId}")
-    public EmployeeDTO updateEmployeeById(@RequestBody EmployeeDTO employeeDTO , @PathVariable Long employeeId){
+    public ResponseEntity<EmployeeDTO> updateEmployeeById(@RequestBody EmployeeDTO employeeDTO , @PathVariable Long employeeId){
 
-        return employeeService.updateEmployeeById(employeeId,employeeDTO);
+        return ResponseEntity.ok(employeeService.updateEmployeeById(employeeId,employeeDTO));
     }
 
     //Delete mapping
     @DeleteMapping("/{employeeId}")
-    public boolean deleteEmployeeById(@PathVariable Long employeeId){
-       return  employeeService.deleteEmployeeById(employeeId);
+    public ResponseEntity<Boolean> deleteEmployeeById(@PathVariable Long employeeId){
+        boolean gotdeleted = employeeService.deleteEmployeeById(employeeId);
+        if(gotdeleted) return ResponseEntity.ok(true);
+        else return ResponseEntity.notFound().build();
+
     }
     // update the partial data
     @PatchMapping(path  = "/{employeeId}")
-    public EmployeeDTO updatePartialEmployeeId(@RequestBody Map<String, Object> updates, @PathVariable Long employeeId){
-        return employeeService.updatePartialEmployeeById(employeeId,updates);
+    public ResponseEntity<EmployeeDTO> updatePartialEmployeeId(@RequestBody Map<String, Object> updates, @PathVariable Long employeeId){
+        EmployeeDTO employeeDTO=employeeService.updatePartialEmployeeById(employeeId,updates);
+        if(employeeDTO==null) return  ResponseEntity.notFound().build();
+        return ResponseEntity.ok(employeeDTO);
     }
 
 
--- Restful_Api/src/main/java/com/Rest_api/Restful_Api/services/EmployeeService.java
+++ Restful_Api/src/main/java/com/Rest_api/Restful_Api/services/EmployeeService.java
@@ -10,6 +10,7 @@ import org.springframework.util.ReflectionUtils;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,10 +30,11 @@ public class EmployeeService {
 
 
 
-    public EmployeeDTO getEmployeeById(Long id) {
-       EmployeeEntity employeeEntity= employeeRepository.findById(id).orElse(null);
-
-        return modelMapper.map(employeeEntity,EmployeeDTO.class);
+    public Optional<EmployeeDTO> getEmployeeById(Long id) {
+//       Optional<EmployeeEntity> employeeEntity= employeeRepository.findById(id);
+//
+//        return employeeEntity.map(employeeEntity1 -> modelMapper.map(employeeEntity1,EmployeeDTO.class));
+        return employeeRepository.findById(id).map(employeeEntity -> modelMapper.map(employeeEntity,EmployeeDTO.class));
     }
 
     public List<EmployeeDTO> getAllEmployees() {
